c021e33c19f15606d2114d1f31977722117ab3e0
Leonardo <leonardozecchin3@gmail.com>
2023-08-31 10:46:27 +0200

    Added countDeltaT

--- countDeltaT.py
+++ countDeltaT.py
@@ -23,6 +23,53 @@ def sortDf(df):
         return None
     return df
 
+def getDeltaT(df):
+    dfNoNan = df.dropna().reset_index(drop=True)
+    deltaT = []
+    for c in range(len(dfNoNan)-1):
+        date1 = dfNoNan.loc[c]['dt']
+        date2 = dfNoNan.loc[c+1]['dt']
+        delta = date2 - date1
+        delta = delta.total_seconds()
+        deltaT.append(delta)
+    deltaT = np.array(deltaT)
+    avg_deltaT = deltaT.mean()
+    days = avg_deltaT / 86400
+    return days
+
+def getKeysToDel(dictionary):
+    keys_to_delete = []
+    keys_to_delete.append('TIMESTAMP')
+    keys_to_delete.append('dt')
+    for key in dictionary.keys():
+        if len(dictionary[key]) <= 2:
+            keys_to_delete.append(key)
+    return keys_to_delete
+
+def getDictDeltaT(df):
+    dict_deltaT = {}
+    for col in df.columns:
+        dict_deltaT[col] = None
+
+    for col in df.columns:
+        if col != 'dt' and col != 'TIMESTAMP':
+            # print(col)
+            df_col = df[[col,'dt']]
+            days = getDeltaT(df_col)
+            dict_deltaT[col] = days
+
+    keys_to_delete = getKeysToDel(dict_deltaT)
+    for key in keys_to_delete:
+        del dict_deltaT[key]
+
+    return dict_deltaT
+
+def getAvgDeltaT(dict_deltaT):
+    arr_values = list(dict_deltaT.values())
+    arr_values = np.array(arr_values)
+    avg_deltaT = arr_values.mean()
+
+    return avg_deltaT
 
 if __name__ == '__main__':
 
@@ -41,6 +88,7 @@ if __name__ == '__main__':
     df_sorted = sortDf(df)
     df_sorted = cleanCSV(df_sorted)
 
-    df_caldaia = df_sorted[['Caldaia','dt']]
-    
-    print(df_caldaia.head())
+    dict_deltaT = getDictDeltaT(df_sorted)
+    avg_deltaT = getAvgDeltaT(dict_deltaT)
+
+    print("Average deltaT:", avg_deltaT, "days")
